a15d1d34e50e6dd68b39911572860c19c566cedd
Mr. A <andikawahyusyaputra@gmail.com>
2023-08-03 18:07:02 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+// app
+const app = express();
+
+// db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then(() => console.log("DB CONNECTED"))
+  .catch((error) => console.log(`DB CONNECTION ERROR: ${error}`));
+
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "2mb" }));
+app.use(cors());
+
+// routes
+app.get("/api", (req, res) => {
+  res.json({ data: "you hit node API" });
+});
+
+// port
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`SERVER IS RUNNING ON PORT ${port}`));
